38d3069c90bb7c410a76334cd4f23888d788ceb3
shaiadul <devshaiadul69@gmail.com>
2024-02-29 23:03:14 +0600

    dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -8,15 +8,15 @@ const Dashboard = () => {
   const router = useRouter();
   const { user } = UserAuth();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const { localStorage } = window;
-      const guard = localStorage.getItem("token");
-      if (!guard && !user) {
-        router.push("/authentication/signin");
-      }
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const { localStorage } = window;
+  //     const guard = localStorage.getItem("token");
+  //     if (!guard && !user) {
+  //       router.push("/authentication/signin");
+  //     }
+  //   }
+  // }, []);
 
   useEffect;
   return (
