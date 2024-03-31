f03d78555fb41fdb56b7a6c62745c13cbe7dcfae
shaiadul <devshaiadul69@gmail.com>
2024-04-01 00:31:01 +0600

    medication record

--- app/dashboard/medicationrecord/page.jsx
+++ app/dashboard/medicationrecord/page.jsx
@@ -1,8 +1,28 @@
-import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+"use client";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 import DashCard from "../components/dashCard";
 
 export default function MedicationRecord() {
+  const [medicationRecords, setMedicationRecords] = useState([]);
+
+  useEffect(() => {
+    fetch("https://tele.syscomatic.com/api/v1/medication/getAllMedicationRecords")
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          setMedicationRecords(data.data);
+          console.log('Medication records:', data.data);
+        } else {
+          console.error('Invalid data format:', data);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching medication records:', error);
+      });
+  }, []);
+
   return (
     <section>
       <DashCard title="Medication Record" />
@@ -19,88 +39,36 @@ export default function MedicationRecord() {
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white border-b border-">
-                <td className="py-2 px-4">Zahed Hasan</td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    style={{ color: "#63E6BE" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-              </tr>
-              <tr className="bg-white border-b border-">
-                <td className="py-2 px-4">Tasin</td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    style={{ color: "#63E6BE" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-              </tr>
-              <tr className="bg-white border-b border-">
-                <td className="py-2 px-4">Amena Khatun</td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faXmark}
-                    style={{ color: "#FF0000" }}
-                  />
-                </td>
-                <td className="py-2 px-4 text-xl">
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    style={{ color: "#63E6BE" }}
-                  />
-                </td>
-              </tr>
+              {medicationRecords.map((record, index) => (
+                <tr key={index} className="bg-white border-b border-">
+                  <td className="py-2 px-4">{record.medicationName}</td>
+                  <td className="py-2 px-4 text-xl">
+                    <FontAwesomeIcon
+                      icon={record.morning ? faCheck : faTimes}
+                      style={{ color: record.morning ? "#63E6BE" : "#FF0000" }}
+                    />
+                  </td>
+                  <td className="py-2 px-4 text-xl">
+                    <FontAwesomeIcon
+                      icon={record.afternoon ? faCheck : faTimes}
+                      style={{ color: record.afternoon ? "#63E6BE" : "#FF0000" }}
+                    />
+                  </td>
+                  <td className="py-2 px-4 text-xl">
+                    <FontAwesomeIcon
+                      icon={record.night ? faCheck : faTimes}
+                      style={{ color: record.night ? "#63E6BE" : "#FF0000" }}
+                    />
+                  </td>
+                  <td className="py-2 px-4 text-xl">
+                    {/* <FontAwesomeIcon
+                      icon={record.remainingDays > 0 ? faCheck : faTimes}
+                      style={{ color: record.remainingDays > 0 ? "#63E6BE" : "#FF0000" }}
+                    /> */}
+                    {record.dayRemaining}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
